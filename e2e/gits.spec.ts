17740cbfe5e8862da94b868057342e127b1d50b0
dhivyamanikanda <dhivyamanikandan1@gmail.com>
2023-11-13 22:36:35 +0100

    Update gits.spec.ts

--- e2e/gits.spec.ts
+++ e2e/gits.spec.ts
@@ -8,8 +8,8 @@ async function login(page) {
   console.log(val);
 
   await page.goto('https://github.com/login');
-  await page.locator('[id="login_field"]').fill(user.trim());
-  await page.locator('[id="password"]').fill(val.trim());
+  await page.locator('[id="login_field"]').fill('dhivyamanikandan1@gmail.com');
+  await page.locator('[id="password"]').fill('Chellam!1984');
   await page.locator('[name="commit"]').click();
   await page.screenshot({ path: 'screenshot.png' });
   await page.locator('span > img').click();
